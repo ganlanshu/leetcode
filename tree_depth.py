0f52ad19094c112ca50b1076276ddc7feaf5cc0c
ganlanshu <grapebook0@163.com>
2018-11-13 20:25:34 +0800

    check if a tree balanced

--- tree_depth.py
+++ tree_depth.py
@@ -96,3 +96,63 @@ class Solution(object):
                     queue.append(right)
             depth += 1
         return depth
+
+    def isBalanced(self, root):
+        if not root:
+            return True
+        left = root.left
+        right = root.right
+        if abs(self.maxDepth(left) - self.maxDepth(right)) > 1:
+            return False
+        return self.isBalanced(left) and self.isBalanced(right)
+
+    def isBalanced1(self, root):
+        balanced = [True]
+        def _get_tree_depth(root, balanced):
+            if not root:
+                return 0
+            depth_l = _get_tree_depth(root.left, balanced)
+            depth_r = _get_tree_depth(root.right, balanced)
+            if abs(depth_l - depth_r) > 1:
+                balanced[0] = False
+            return 1 + max(depth_l, depth_r)
+        depth = _get_tree_depth(root, balanced)
+        if not balanced[0]:
+            return False
+        return True
+
+    def isBalanced2(self, root):
+        return self._depth(root) != -1
+
+    def _depth(self, root):
+        """
+        当左右子树高度差大于1,返回-1,其他时候返回树的高度
+        :param root:
+        :return:
+        """
+        if not root:
+            return 0
+        depth_l = self._depth(root.left)
+        depth_r = self._depth(root.right)
+        # 左子树不平衡
+        if depth_l == -1:
+            return -1
+        # 右子树不平衡
+        if depth_r == -1:
+            return -1
+        if abs(depth_l - depth_r) > 1:
+            return -1
+        return 1 + max(depth_l, depth_r)
+
+    def isBalanced3(self, root):
+        depth, balanced = self.get_depth(root)
+        return balanced
+
+    def get_depth(self, root):
+        if not root:
+            return 0, True
+        dl, bl = self.get_depth(root.left)
+        dr, br = self.get_depth(root.right)
+        h = 1 + max(dl, dr)
+        b = bl and br and abs(dl-dr) < 2
+        return h, b
