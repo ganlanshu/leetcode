c4f0f0037760725bbb32002a44709ab68f82df8c
ganlanshu <grapebook0@163.com>
2018-11-14 11:34:07 +0800

    determine if the tree has a root-to-leaf path such that adding up to a given sum

--- tree_path.py
+++ tree_path.py
@@ -26,3 +26,35 @@ class Solution(object):
             if child:
                 result += self.binaryTreePaths(child)
         return [str(root.val) + '->' + value for value in result]
+
+    def hasPathSum(self, root, sum):
+        """
+        112. Path Sum
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if not root.left and not root.right:
+            if sum == root.val:
+                return True
+            return False
+        if self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val):
+            return True
+        return False
+
+    def hasPathSum1(self, root, sum):
+        if not root:
+            return False
+        stack = []
+        stack.append((root, sum))
+        while stack:
+            top, _sum = stack.pop()
+            if not top.left and not top.right and top.val == _sum:
+                return True
+            if top.right:
+                stack.append((top.right, _sum-top.val))
+            if top.left:
+                stack.append((top.left, _sum-top.val))
+        return False
