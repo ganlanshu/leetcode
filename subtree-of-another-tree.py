cecac0037067ea7a04f8a18bc7d522c745ffce40
ganlanshu <grapebook0@163.com>
2018-11-15 23:15:13 +0800

    check a substructure of a tree

--- subtree-of-another-tree.py
+++ subtree-of-another-tree.py
@@ -37,3 +37,24 @@ class Solution(object):
         def convert(s):
             return '^'+str(s.val)+'#' + convert(s.left) + convert(s.right) if s else '$'
         return convert(t) in convert(s)
+
+    def is_substructure(self, s, t):
+        """
+        子结构和子树不一样,看辅助方法就知道,子树需要完全一样
+        子结构只要部分一样
+        判断t是否是s的子结构,参考
+        https://blog.csdn.net/qq_33431368/article/details/79257029
+        :param s:
+        :param t:
+        :return:
+        """
+        if not s or not t:
+            return False
+        return self._substructure(s, t) or self.is_substructure(s.left, t) or self.is_substructure(s.right, t)
+
+    def _substructure(self, root1, root2):
+        if not root2:
+            return True
+        if not root1:
+            return False
+        return root1.val == root2.val and self._substructure(root1.left, root2.left) and self._substructure(root1.right, root2.right)
