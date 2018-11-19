28e2c8ffa1a2782142e8b57023e9b8ab93e6a672
ganlanshu <grapebook0@163.com>
2018-11-19 12:28:17 +0800

    check a linklist palindrome

--- palindrome-linked-list.py
+++ palindrome-linked-list.py
@@ -11,17 +11,41 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        pass
+        if not head:
+            return True
+        if not head.next:
+            return True
+        current = head
+        mid_node = self.find_middlenode_and_slice(current)
+        right_link = self._reverse_linklist(mid_node)
+        left_link = current
+        while left_link and right_link:
+            if left_link.val != right_link.val:
+                return False
+        return True
 
     def _reverse_linklist(self, head):
         pre = None
-        current = head
-        while current:
-            temp = current.next
-            current.next = pre
-            pre = current
-            current = temp
+        while head:
+            temp = head.next
+            head.next = pre
+            pre = head
+            head = temp
         return pre
+
+    def find_middlenode_and_slice(self, head):
+        """
+        判断是否是回文时,找到中间节点,把中间节点以前的阶段
+        :param head:
+        :return:
+        """
+        slow = fast = pre = head
+        while fast and fast.next:
+            fast = fast.next.next
+            pre = slow
+            slow = slow.next
+        pre.next = None
+        return slow
 
     def middleNode(self, head):
         """
