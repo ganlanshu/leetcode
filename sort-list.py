8a5e26c055846bc21cfc128df94983273d1353df
ganlanshu <grapebook0@163.com>
2018-11-20 22:20:01 +0800

    merge sort a link list

---
+++ sort-list.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+
+"""
+Sort a linked list in O(n log n) time using constant space complexity.
+"""
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+
+    def merge_sort(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return
+        if not head.next:
+            return head
+        mid_node = self.get_mid_node(head)
+        right = mid_node.next
+        mid_node.next = None
+        left = head
+        return self.merge_two_sorted_list(self.merge_sort(left), self.merge_sort(right))
+
+    def merge_two_sorted_list(self, l1, l2):
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        dummy = ListNode(0)
+        pre = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                pre.next = l1
+                l1 = l1.next
+            else:
+                pre.next = l2
+                l2 = l2.next
+            pre = pre.next
+        if l1:
+            pre.next = l1
+        if l2:
+            pre.next = l2
+        return dummy.next
+
+
+    def get_mid_node(self, head):
+        if not head:
+            return
+        if not head.next:
+            return head
+        fast = head.next
+        slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
