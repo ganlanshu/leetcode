0d49c65b4a7da4be3c164bcd91319a2b9c6b3fb3
ganlanshu <grapebook0@163.com>
2018-11-26 18:40:17 +0800

    O(n) time shortest-unsorted-continuous-subarray

--- shortest-unsorted-continuous-subarray.py
+++ shortest-unsorted-continuous-subarray.py
@@ -1,10 +1,12 @@
 # coding=utf-8
+import copy
 
 """
 581. Shortest Unsorted Continuous Subarray
 """
-import copy
+
 class Solution(object):
+
     def findUnsortedSubarray(self, nums):
         """
         :type nums: List[int]
@@ -30,3 +32,28 @@ class Solution(object):
             else:
                 break
         return end-begin+1
+
+    def findUnsortedSubarray1(self, nums):
+        # 详细解释参考
+        # https://leetcode.com/problems/shortest-unsorted-continuous-subarray/discuss/103066/Ideas-behind-the-O(n)-two-pass-and-one-pass-solutions
+        if not nums:
+            return 0
+        begin = 0
+        end = len(nums) - 1
+        while begin < end and nums[begin] <= nums[begin+1]:
+            begin += 1
+        if begin == end:
+            return 0
+        while nums[end] >= nums[end-1]:
+            end -= 1
+        max_num = float('-inf')
+        min_num = float('inf')
+        for k in range(begin, end+1):
+            max_num = max(max_num, nums[k])
+            min_num = min(min_num, nums[k])
+
+        while begin >= 0 and nums[begin] > min_num:
+            begin -= 1
+        while end < len(nums) and nums[end] < max_num:
+            end += 1
+        return end - begin - 1
