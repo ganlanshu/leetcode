5c9da3439a2123deb2b5267fc63b06e5ea3f1ea8
ganlanshu <grapebook0@163.com>
2018-12-04 17:17:18 +0800

    subsets of array

--- subsets.py
+++ subsets.py
@@ -22,3 +22,25 @@ class Solution(object):
         subset_post.extend(new_subset)
         return subset_post
 
+    def subsets1(self, nums):
+        if not nums:
+            return []
+        result = [[nums[0]], []]
+        n = len(nums)
+        for i in range(1, n):
+            new_subset = [elem + [nums[i]] for elem in result]
+            result.extend(new_subset)
+        return result
+
+    def subset2(self, nums):
+        res = []
+        n = len(nums)
+
+        def dfs(path, index):
+            res.append(path)
+
+            for i in range(index, n):
+                dfs(path+[nums[i]], i+1)
+
+        dfs([], 0)
+        return res
