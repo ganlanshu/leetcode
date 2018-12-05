71344e9e23b9bb07fd71ca7a92622520983cb54b
ganlanshu <grapebook0@163.com>
2018-12-05 15:21:29 +0800

    unique path

--- unique-paths.py
+++ unique-paths.py
@@ -21,3 +21,23 @@ class Solution(object):
 
 
 
+    def uniquePaths1(self, m, n):
+        """
+        典型的动态规划 dp[i,j] = dp[i-1,j]+dp[i,j-1]
+        :param m:
+        :param n:
+        :return:
+        """
+        if m < 1 or n < 1:
+            return 0
+        if m == 1 or n == 1:
+            return 1
+        # 先做出一个m*n的矩阵
+        result = [
+            [1]*n
+        ]
+        result *= m
+        for i in range(1, m):
+            for j in range(1, n):
+                result[i][j] = result[i-1][j] + result[i][j-1]
+        return result[m-1][n-1]
