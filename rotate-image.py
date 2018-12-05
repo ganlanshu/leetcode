c68369a2703b65fb2b884bf6efd5ea4d82f700d4
ganlanshu <grapebook0@163.com>
2018-12-05 19:58:51 +0800

    rotate image clockwise 90

---
+++ rotate-image.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+"""
+48. Rotate Image
+You are given an n x n 2D matrix representing an image.
+Rotate the image by 90 degrees (clockwise).
+"""
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n//2):
+            for j in range(i, n-1-i):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[n-1-j][i]
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+                matrix[j][n-1-i] = temp
