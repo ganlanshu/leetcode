eee39ffe5a92003afa683e92939a965c690f2ef4
ganlanshu <grapebook0@163.com>
2018-12-10 23:15:36 +0800

    binary search when sorted array is rotated

---
+++ search-in-rotated-sorted-array.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+"""
+33. Search in Rotated Sorted Array
+"""
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (left+right)//2
+            if target == nums[mid]:
+                return mid
+            if nums[mid] < nums[right]:  # 右边是有序的
+                if nums[mid] < target <= nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
+            else:  # 左边是有序的
+                if nums[left] <= target < nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid+1
+        return -1
