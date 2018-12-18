0ac4e527e724117292e74adba32783cdde8a71ea
ganlanshu <grapebook0@163.com>
2018-12-18 16:51:36 +0800

    第n个ugly number

--- ugly-number.py
+++ ugly-number.py
@@ -34,3 +34,42 @@ class Solution(object):
                 num = num / x
         return num == 1
 
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        264. Ugly Number II
+        Write a program to find the n-th ugly number.
+        Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+        """
+        if n <= 5:
+            return n
+        ugly_num_set = set()
+        # 1到5 这几个ugly number先放入集合
+        for i in range(1, 6):
+            ugly_num_set.add(i)
+        number = 6
+        ugly_count = 5
+        while ugly_count < n:
+            if number % 2 == 0:
+                if number/2 in ugly_num_set:
+                    ugly_num_set.add(number)
+                    ugly_count += 1
+                number += 1
+                continue
+            if number % 3 == 0:
+                if number/3 in ugly_num_set:
+                    ugly_num_set.add(number)
+                    ugly_count += 1
+                number += 1
+                continue
+            if number % 5 == 0:
+                if number/5 in ugly_num_set:
+                    ugly_num_set.add(number)
+                    ugly_count += 1
+                number += 1
+                continue
+            else:
+                number += 1
+        return number-1
+
