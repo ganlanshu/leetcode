a5356da61253a419602c342ed137feb7fd1bdfd6
ganlanshu <grapebook0@163.com>
2018-12-26 12:47:03 +0800

    sliding window maximum

--- sliding-window-maximum.py
+++ sliding-window-maximum.py
@@ -4,6 +4,7 @@
 
 """
 class Solution(object):
+
     def maxSlidingWindow(self, nums, k):
         """
         :type nums: List[int]
@@ -18,4 +19,25 @@ class Solution(object):
         for i in range(n-k+1):
             res.append(max(nums[i:i+k]))
         return res
+
+    def maxSlidingWindow1(self, nums, k):
+        """
+        :param nums:
+        :param k:
+        :return:
+        看了提示,用deque来做
+        """
+        from collections import deque
+        if not nums:
+            return []
+        queue = deque(nums[:k])
+        res = []
+        res.append(max(queue))
+        n = len(nums)
+        for i in range(k, n):
+            queue.popleft()
+            queue.append(nums[i])
+            res.append(max(queue))
+        return res
+
 
