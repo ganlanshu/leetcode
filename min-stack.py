af936036baaa89d6dd0b1f08c275843819e074ce
ganlanshu <grapebook0@163.com>
2018-12-28 12:29:31 +0800

    min-stack using a stack

--- min-stack.py
+++ min-stack.py
@@ -6,6 +6,9 @@ and retrieving the minimum element in constant time.
 """
 
 class MinStack(object):
+    """
+    用2个栈来实现
+    """
 
     def __init__(self):
         """
@@ -49,6 +52,53 @@ class MinStack(object):
         """
         return self.min_stack[-1]
 
+class MinStack1(object):
+    """
+    用一个栈来实现, 入栈的时候存的不是push的元素本身,而是该元素与最小值的差值
+    """
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = None
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if not self.stack:
+            self.stack.push(0)
+            self.min = x
+        else:
+            self.stack.push(x-self.min)
+            self.min = min(x, self.min)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if not self.stack:
+            return
+        pop = self.stack.pop()
+        if pop < 0:
+            self.min = self.min-pop
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.stack[-1] < 0:
+            return self.min
+        else:
+            return self.min + self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_stack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
