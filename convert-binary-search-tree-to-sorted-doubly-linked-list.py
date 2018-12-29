cc9dfa0dfef69c72e0e289f812a7c1eb9f6c1449
ganlanshu <grapebook0@163.com>
2018-12-29 15:22:53 +0800

    convert binary search tree to sorted doubly linked list

--- convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -11,7 +11,13 @@ class TreeNode(object):
         self.right = None
 
 class Solution(object):
-    def convert(self, root):
+
+    def bst_to_doubly_link(self, root):
+        """
+        这是最先想到的,非递归方法
+        :param root:
+        :return:
+        """
         if not root:
             return
         stack = []
@@ -32,6 +38,53 @@ class Solution(object):
                 pre = top
         return head
 
+    def bst_to_doubly_link1(self, root):
+        """
+        用递归方法试试
+        :param root:
+        :return:
+        """
+        if not root:
+            return
+        if not root.left and not root.right:
+            return root
+        # 对左子树递归,得到左子树转为doubly link的头结点
+        left = self.bst_to_doubly_link1(root.left)
+        tail = left
+        # 从头结点找到尾节点
+        while tail and tail.right:
+            tail = tail.right
+        # 有的根节点没有左子树,这是tail就为空
+        if tail:
+            tail.right = root
+            root.left = tail
+        right = self.bst_to_doubly_link1(root.right)
+        if right:
+            root.right = right
+            right.left = root
+        return left if left else root
 
+
+    def bst_to_doubly_link2(self, root):
+        last_node = None
+        self._convert_node(root, last_node)
+        # 找到链表最右边的节点,再从这里遍历到头结点
+        head = last_node
+        while head and head.left:
+            head = head.left
+        return head
+
+    def _convert_node(self, root, last_node):
+        if not root:
+            return
+        current = root
+        if current.left:
+            self._convert_node(current.left, last_node)
+        current.left = last_node
+        if last_node:
+            last_node.right = current
+        last_node = current
+        if current.right:
+            self._convert_node(current.right, last_node)
 
 
