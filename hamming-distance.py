41aa0e0cc03f4ee6a4eb6360fe9af45adc99653b
ganlanshu <grapebook0@163.com>
2019-10-22 22:09:15 +0800

    hamming distance using bit map

---
+++ hamming-distance.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+"""
+461. Hamming Distance
+"""
+
+class Solution(object):
+
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        # 同一个bit不相等的个数，先求异或运算，再数数结果中bit为1的个数
+        z = x ^ y
+        count = 0
+        while z:
+            count += z & 1
+            z >>= 1
+        return count
+
+
