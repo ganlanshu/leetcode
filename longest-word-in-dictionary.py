9a318b0dec823e4951e973b793a2bd0c699977f4
ganlanshu <grapebook0@163.com>
2019-10-26 23:07:38 +0800

    longest-word-in-dictionary

---
+++ longest-word-in-dictionary.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+"""
+720. Longest Word in Dictionary
+"""
+
+
+class Solution(object):
+
+    def longestWord(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        ans = ''
+        word_set = set(words)
+        words.sort(key=lambda c: (-len(c), c))
+        # 先排序，若长度最长的word的prefix在word_set里都有，就有了
+        for word in words:
+            if all(word[:k] in word_set for k in range(1, len(word))):
+                return word
+        return ans
+
+    def longest_word(self, words):
+
